21f137af8dc8886c50f3419f70f89bd75f3916bc
heyjoeway <joseph14078@gmail.com>
2021-02-01 21:25:52 -0500

    buh

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # micropython-lightswitcher
-A quick n' dirty micropython script to control light switches using MQTT (to be paired with Home Assistant)
+A quick n' dirty micropython script to control light switches using MQTT and servos. (to be paired with Home Assistant)
+For the physical part of this project (not done by me) see here: https://www.thingiverse.com/thing:1156995
+
+## Config
+This assumes you know how to set up Home Assistant and Mosquitto (or another compatible MQTT server). You'll also need to have already flashed MicroPython to whatever device you're using.
+
+## Home Assistant Config Example
+Loosely follows the included `settings.example.json`:
+```yaml
+switch:
+  - platform: mqtt
+    unique_id: example_switch1
+    name: "Example Switch"
+    state_topic: "example/switch1/state"
+    command_topic: "example/switch1/set"
+    availability:
+      - topic: "example/switch1/available"
+    payload_on: "ON"
+    payload_off: "OFF"
+    state_on: "ON"
+    state_off: "OFF"
+    optimistic: false
+    qos: 0
+    retain: true
+```
+
+### Micropython
+Upload `settings.json` (use `settings.example.json` as a guide) using WebREPL (http://micropython.org/webrepl/).
+Then upload `main.py`.
---
+++ main.py
@@ -0,0 +1,91 @@
+import ujson
+
+settings = None
+with open("settings.json", 'r') as f:
+    settings = ujson.loads(f.read())
+
+# =============================================================================
+
+import network
+import time
+
+sta_if = None
+def wifi_connect():
+    global sta_if
+    sta_if = network.WLAN(network.STA_IF); sta_if.active(True)
+    sta_if.connect(settings["wifi"]["ssid"], settings["wifi"]["pass"]) # Connect to an AP
+    
+    for i in range(20):
+        if sta_if.isconnected():
+            return
+        time.sleep(1)
+
+def wifi_ensure_connected():
+    while not sta_if.isconnected():
+        wifi_connect()
+
+wifi_connect()
+
+import webrepl
+
+# =============================================================================
+
+import machine
+pin = machine.Pin(settings["servo"]["pin"])
+pwm = machine.PWM(pin)
+pwm.freq(50)
+
+# Power on self test (optional)
+#pwm.duty(settings["servo"]["duty_on"])
+#time.sleep(1)
+#pwm.duty(0)
+#time.sleep(.1)
+#pwm.duty(settings["servo"]["duty_off"])
+#time.sleep(1)
+#pwm.duty(0)
+#time.sleep(.1)
+
+# =============================================================================
+
+from umqtt.robust import MQTTClient
+
+def mqtt_callback(topic, payload):
+    print((topic, payload))
+    topic = topic.decode('utf-8')
+    payload = payload.decode('utf-8')
+    if topic == settings["mqtt"]["topic"] + settings["mqtt"]["subtopic_set"]:
+        if payload == "ON":
+            print("Turning on")
+            mqtt.publish(settings["mqtt"]["topic"] + settings["mqtt"]["subtopic_state"], "ON")
+            pwm.duty(settings["servo"]["duty_on"])
+            time.sleep(0.5)
+            pwm.duty(settings["servo"]["duty_mid"])
+            time.sleep(0.5)
+            pwm.duty(0)
+        elif payload == "OFF":
+            print("Turning off")
+            mqtt.publish(settings["mqtt"]["topic"] + settings["mqtt"]["subtopic_state"], "OFF")
+            pwm.duty(settings["servo"]["duty_off"])
+            time.sleep(0.5)
+            pwm.duty(settings["servo"]["duty_mid"])
+            time.sleep(0.5)
+            pwm.duty(0)
+
+mqtt = MQTTClient(
+    settings["mqtt"]["clientId"],
+    settings["mqtt"]["host"],
+    user=settings["mqtt"]["user"],
+    password=settings["mqtt"]["pass"],
+    port=settings["mqtt"]["port"]
+)
+mqtt.set_callback(mqtt_callback)
+mqtt.connect()
+mqtt.subscribe(settings["mqtt"]["topic"] + settings["mqtt"]["subtopic_set"])
+
+mqtt.publish(settings["mqtt"]["topic"] + settings["mqtt"]["subtopic_state"], "ON")
+mqtt.publish(settings["mqtt"]["topic"] + settings["mqtt"]["subtopic_avail"], "online")
+
+while True:
+    wifi_ensure_connected()
+    mqtt.check_msg()
+    time.sleep(0.25)
